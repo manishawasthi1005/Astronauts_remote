25cda2aa8e6a3540d2c94268b9365411a060a2b4
m.a.awasthi <Manish.Awasthi@externals.adidas.com>
2023-05-02 13:47:07 +0200

    Initial Commit

---
+++ Astronauts/AstronautsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AstronautsApp.swift
+//  Astronauts
+//
+//  Created by Manish Awasthi on 02.05.23.
+//
+
+import SwiftUI
+
+@main
+struct AstronautsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
